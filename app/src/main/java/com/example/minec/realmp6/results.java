43c262e8fb8da27f6ca761761a3d2f3dd7a5e21c
clopez94 <clopez94@illinois.edu>
2018-12-09 14:11:13 -0600

    WIP, most stuff is done

--- app/src/main/java/com/example/minec/realmp6/results.java
+++ app/src/main/java/com/example/minec/realmp6/results.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -25,6 +27,16 @@ public class results extends AppCompatActivity {
             HttpURLConnection connection = (HttpURLConnection)u.openConnection();
             JsonReader reader = new JsonReader(new InputStreamReader(connection.getInputStream()));
             ArrayList<String> data = new ArrayList<String>();
+            reader.beginArray();
+            while(reader.hasNext()) {
+                reader.beginObject();
+                data.add(reader.nextString());
+                reader.endObject();
+            }
+            reader.endArray();
+            ListView listView = (ListView)findViewById(R.id.results);
+            ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.listitem, R.id.viewer, data);
+            listView.setAdapter(adapter);
         } catch (Exception ex) {}
     }
 }
