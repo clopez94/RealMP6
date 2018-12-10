8703ad112d9f8863ed684965c94340fd84e14600
clopez94 <clopez94@illinois.edu>
2018-12-10 11:27:28 -0600

    App is DONE

--- app/src/main/java/com/example/minec/realmp6/MainActivity.java
+++ app/src/main/java/com/example/minec/realmp6/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText search;
     private final static String API = "https://nflarrest.com/api/v1/";
     public final static String KEY_URL = "com.example.minec.realmp6.URL";
     public final static String KEY_TAG = "com.example.minec.realmp6.TAG";
@@ -15,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        search = (EditText)findViewById(R.id.editText);
     }
     public void resultsToDisplay(String url, String tag) {
         Intent intent = new Intent(this, results.class);
@@ -36,16 +41,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void league_leader_arrests(View view) {
-
+        String store;
+        String editTextStore;
+        try {
+            editTextStore = URLEncoder.encode(search.getText().toString(), "UTF-8");
+            store = API + "crime/topPlayers/" + editTextStore + "?limit=10";
+            resultsToDisplay(store,"Name");
+        } catch (Exception e) {}
     }
 
     public void position_top_crimes(View view) {
-
+        String store;
+        String editTextStore;
+        try {
+            editTextStore = URLEncoder.encode(search.getText().toString(), "UTF-8");
+            store = API + "position/topCrimes/" + editTextStore + "?limit=10";
+            resultsToDisplay(store,"Category");
+        } catch (Exception e) {}
     }
 
     public void crimes_by_position(View view) {
         String url = API + "position?limit=10";
         String tag = "Position";
         resultsToDisplay(url, tag);
+    }
+    public void player_search(View view) {
+        String store;
+        String editTextStore;
+        try {
+            editTextStore = URLEncoder.encode(search.getText().toString(), "UTF-8");
+            store = API + "player/arrests/" + editTextStore;
+            resultsToDisplay(store,"Category");
+        } catch (Exception e) {}
     }
 }
